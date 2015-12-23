b22fdef2f4f93113d50e56734b1bcccb09aa24bb
mid-eum kim <mid-eum kim>
2015-12-23 23:23:04 +0900

    start add debounce

--- switchListener.c
+++ switchListener.c
@@ -1,3 +1,9 @@
+/* for debouncedBtnRead */
+#define BREAK 0
+#define LEFT 1
+#define RIGHT 2
+
+/* for signal control, they needs check by four ways */
 #define BT_STATE_OFF 0  // LOW -> LOW
 #define BT_STATE_UP 1   // LOW -> HIGH
 #define BT_STATE_ON 2   // HIGH -> HIGH
@@ -9,26 +15,37 @@
 #define LED_STATE_OFF 0
 #define LED_STATE_ON 1
 
-int btBreakState = BT_STATE_OFF;
-int btLeftState = BT_STATE_OFF;
-int btRightState = BT_STATE_OFF;
+/* for debouncedBtnRead */
+int btnVolts[3] = {LOW,LOW,LOW};
+int lastBtnState[3] = {LOW,LOW,LOW};
+int curBtnState[3] = {LOW,LOW,LOW};
+long lastDebouncedTime_ms[3] = {0, 0, 0};
+long debounceDelay = 50;
+
+// int btBreakState = BT_STATE_OFF;
+// int btLeftState = BT_STATE_OFF;
+// int btRightState = BT_STATE_OFF;
 
 int leftSignalState=SIG_OFF;
 int rightSignalState=SIG_OFF;
 
-// int debouncedReadButton(int PIN_NUM) {
+// int debouncedBtnRead(int PIN_NUM) {
 //   return digitalRead(PIN_NUM);
 // }
 
-// check all button's state using debouncedReadButton
+// check all button's state using debouncedBtnRead
 int checkButtonState(int PIN_NUM) {
   return digitalRead(PIN_NUM);
 }
 
 void updateButtonState() {
-  btBreakState = checkButtonState(SW_BREAK);
-  btLeftState = checkButtonState(SW_LEFT);
-  btRightState = checkButtonState(SW_RIGHT);
+  int i;
+  for(i=0;i<3;i++) {
+    debouncedBtnRead(i);
+  }
+  // btBreakState = checkButtonState(SW_BREAK);
+  // btLeftState = checkButtonState(SW_LEFT);
+  // btRightState = checkButtonState(SW_RIGHT);
 }
 
 void updagteSignal() {
