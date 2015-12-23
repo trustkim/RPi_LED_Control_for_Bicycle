b259522644faef0d1d0aa246f5af28b3901a63d2
mid-eum kim <mid-eum kim>
2015-12-23 23:57:42 +0900

    change swPinIndexes[3]

--- main.h
+++ main.h
@@ -5,8 +5,10 @@
 #define RED_BREAK 1	// pin#right 9th
 #define YELLOW_LEFT 4	// pin# right 8th
 #define YELLOW_RIGHT 5	// pin# right 6th
-#define SW_BREAK 0
-#define SW_LEFT 2
-#define SW_RIGHT 3
+
+int swPinIndexes[3] = {0, 2, 3};
+// #define SW_BREAK 0
+// #define SW_LEFT 2
+// #define SW_RIGHT 3
 
 void callAllPinModes();
