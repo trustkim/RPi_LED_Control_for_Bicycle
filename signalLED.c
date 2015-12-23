492cef7d8c642de4117fab9a48be7026a0102a7c
mid-eum kim <mid-eum kim>
2015-12-24 02:10:02 +0900

    commenting useless code in signalLED

--- signalLED.c
+++ signalLED.c
@@ -1,50 +1,50 @@
 
 const int LED_indexes[2] = {YELLOW_LEFT, YELLOW_RIGHT};
-int LED_States[2] = {LED_STATE_OFF, LED_STATE_OFF};
-// int left_LED_State=LED_STATE_OFF;
-// int right_LED_State=LED_STATE_OFF;
+int LED_States[2] = {LOW, LOW};
+// int left_LED_State=LOW;
+// int right_LED_State=LOW;
 
 void runSignalLED() {
   int i;
   for(i=1;i<3;i++) {
     if(curBtnState[i]==HIGH) {
-      if(LED_States[i-1]==LED_STATE_ON) {
+      if(LED_States[i-1]==HIGH) {
         turnOff(LED_indexes[i-1]);
-        LED_States[i-1]=LED_STATE_OFF;
+        LED_States[i-1]=LOW;
       } else {
         turnOn(LED_indexes[i-1]);
-        LED_States[i-1]=LED_STATE_ON;
+        LED_States[i-1]=HIGH;
       }
     } else {
       turnOff(LED_indexes[i-1]);
-      LED_States[i-1] = LED_STATE_OFF;
+      LED_States[i-1] = LOW;
     }
     // if(signalStates[i]==SIG_ON) {
-    //   if(LED_States[i-1]==LED_STATE_ON) {
+    //   if(LED_States[i-1]==HIGH) {
     //     turnOff(LED_indexes[i-1]);
-    //     LED_States[i-1]=LED_STATE_OFF;
+    //     LED_States[i-1]=LOW;
     //   } else {
     //     turnOn(LED_indexes[i-1]);
-    //     LED_States[i-1]=LED_STATE_ON;
+    //     LED_States[i-1]=HIGH;
     //   }
     // } else {
     //   turnOff(LED_indexes[i-1]);
-    //   LED_States[i-1] = LED_STATE_OFF;
+    //   LED_States[i-1] = LOW;
     // }
   }
   //
   //
   // if(signalStates[index]==SIG_ON) {
-  //   if(right_LED_State==LED_STATE_ON) {
+  //   if(right_LED_State==HIGH) {
   //     turnOff(YELLOW_RIGHT);
-  //     right_LED_State=LED_STATE_OFF;
+  //     right_LED_State=LOW;
   //   } else {
   //     turnOn(YELLOW_RIGHT);
-  //     right_LED_State=LED_STATE_ON;
+  //     right_LED_State=HIGH;
   //   }
   // } else {
   //   turnOff(YELLOW_RIGHT);
-  //   right_LED_State = LED_STATE_OFF;
+  //   right_LED_State = LOW;
   // }
 
   delay(100);
