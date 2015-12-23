c00d40dd7a527acff887f28d2e9b74bd2ae222bd
mid-eum kim <mid-eum kim>
2015-12-23 21:52:58 +0900

    add BT_STATEs

--- switchListener.c
+++ switchListener.c
@@ -1,9 +1,15 @@
-#define BT_STATE_OFF 0
-#define BT_STATE_ON 1
+#define BT_STATE_OFF 0  // LOW -> LOW
+#define BT_STATE_UP 1   // LOW -> HIGH
+#define BT_STATE_ON 2   // HIGH -> HIGH
+#define BT_STATE_DOWN 3 // HIGH -> LOW
 
 int btBreakState = BT_STATE_OFF;
 int btLeftState = BT_STATE_OFF;
 int btRightState = BT_STATE_OFF;
+
+// int debouncedReadButton(int PIN_NUM) {
+//   return digitalRead(PIN_NUM);
+// }
 
 int checkButtonState(int PIN_NUM) {
   return digitalRead(PIN_NUM);
