eea21b78c11757382988016d5209ae17e5406e11
mid-eum kim <mid-eum kim>
2015-12-24 01:56:57 +0900

    fix runSignalLED

--- signalLED.c
+++ signalLED.c
@@ -1,4 +1,5 @@
 
+int LED_indexes[2] = {YELLOW_LEFT, YELLOW_RIGHT};
 int LED_States[2] = {LED_STATE_OFF, LED_STATE_OFF};
 // int left_LED_State=LED_STATE_OFF;
 // int right_LED_State=LED_STATE_OFF;
@@ -8,14 +9,14 @@ void runSignalLED() {
   for(i=1;i<3;i++) {
     if(signalStates[i]==SIG_ON) {
       if(LED_States[i-1]==LED_STATE_ON) {
-        turnOff(swPinIndexes[i]);
+        turnOff(LED_indexes[i-1]);
         LED_States[i-1]=LED_STATE_OFF;
       } else {
-        turnOn(swPinIndexes[i]);
+        turnOn(LED_indexes[i-1]);
         LED_States[i-1]=LED_STATE_ON;
       }
     } else {
-      turnOff(swPinIndexes[i]);
+      turnOff(LED_indexes[i-1]);
       LED_States[i-1] = LED_STATE_OFF;
     }
   }
